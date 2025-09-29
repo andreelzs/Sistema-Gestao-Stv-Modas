f57245b437fd0e1407c4aef4ca0091ab09dcdf2a
André Ferreira <andreelzs@hotmail.com>
2025-09-29 00:35:59 -0300

    .

--- stv modas teste inteiro ia/config/settings.py
+++ stv modas teste inteiro ia/config/settings.py
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [ # Diretórios adicionais onde o Django vai procurar arquivo
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
